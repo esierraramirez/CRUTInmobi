c6eed4fdbd10b990ba897f2d676f0b4975dca010
esierraramirez <essierra62@ucatolica.edu.co>
2024-09-18 19:06:27 -0500

    Correccion user, getter y setters

--- src/main/java/com/example/realestate/model/User.java
+++ src/main/java/com/example/realestate/model/User.java
@@ -12,8 +12,8 @@ import javax.management.relation.Role;
 
 @Entity
 @Table(name = "Users")
-
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,8 +23,49 @@ public class User {
     private String email;
 
     @ManyToOne
-    @JoinColumn(name = "role_id")
+    @JoinColumn(name = "role_id", nullable = false)
     private Role role;
 
+    public User() {}
+
     // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
